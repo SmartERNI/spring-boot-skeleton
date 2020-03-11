b0e0f3ec5295362d5c2f6278dde2d9e3a3d72321
romanboutellier <roman.boutellier@erni.ch>
2020-03-11 08:12:40 +0100

    Rename JSON property of Clock object to isLit

--- src/main/java/com/erni/sbbdemo/model/Clock.java
+++ src/main/java/com/erni/sbbdemo/model/Clock.java
@@ -1,5 +1,6 @@
 package com.erni.sbbdemo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,5 +8,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class Clock {
     private String name;
+    @JsonProperty("isLit")
     private boolean isLit;
 }
